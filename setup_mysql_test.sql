2ec2b2d970a23a48265ac79f53271034763d4eff
malopro <malovagilbert@gmail.com>
2023-08-22 13:22:30 -0700

    fixed sql test db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- Create a new user and new database
+-- Create a new user and new database --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
