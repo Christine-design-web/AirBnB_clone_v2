170fbc641582f90e8d98bdc49dbff8d686b4974d
Christine-design-web <wakuthiichristine5@gmail.com>
2023-09-23 12:58:32 +0300

    changes

--- models/state.py
+++ models/state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This is the state class"""
 import models
-from models.base_model import BaseModel, Base
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from os import getenv
+
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -18,15 +20,17 @@ class State(BaseModel, Base):
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
 
-if getenv('HBNB_TYPE_STORAGE') != 'db':
     @property
     def cities(self):
-        """get a list of all related city instances
-        with state_id = to the current state id
-        """
-        cities_list = []
-
-        for city in list(models.storage.all(City).values()):
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
